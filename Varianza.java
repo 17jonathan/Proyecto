258b9b327ed6ec49c8f484b8aa8faabbcce9612d
Jonathan Chavez <jonechav@espol.edu.ec>
2017-10-27 23:53:18 -0500

    Varianza.java

---
+++ Varianza.java
@@ -0,0 +1,15 @@
+public class Varianza {
+    public static void main(String[] args){
+    double[] nums={11,4,7,5,2,3,9,8,7,10};
+    double sum2=0;
+    double media=6.6;
+    for (int x=0;x<nums.length;x=x+1){
+            sum2=sum2+Math.pow(nums[x]-media,2);        
+        }
+        double varianza=sum2/(nums.length-1);
+        double desviacion=Math.sqrt(varianza);
+        System.out.println("La varianza es: "+varianza);
+        System.out.println("La desviacion es: "+desviacion);
+System.out.println("Gracias por usar Git");
+    } 
+}
