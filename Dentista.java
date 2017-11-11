733af15736797d4aa613bfb048d9fef011f722cb
Jonathan Chavez <jonechav@espol.edu.ec>
2017-11-11 09:33:37 -0500

    Se comenzo a desarrollar el nuevo metodo de Noviembre

--- Dentista.java
+++ Dentista.java
@@ -11,5 +11,9 @@ System.out.println("Estoy trabajando");
 public void metodoNuevo(){
 System.out.println("Soy un metodo nuevo")
 
+public void metodoDeveloper1(){
+System.out.println("Este metodo es de Noviembre del 2017");
+}
+
 }
 }
