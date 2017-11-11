477517eb4353f00319731978bd7d87d5897f2f76
Jonathan Chavez <jonechav@espol.edu.ec>
2017-11-11 09:01:42 -0500

    Se agrego un nuevo valor

--- Dentista.java
+++ Dentista.java
@@ -2,6 +2,7 @@ public class Dentista {
 private String nombre;
 private String cedula;
 private int codigo;
+private String newValue;
 
 public void trabajar(){
 System.out.println("Estoy trabajando");	
@@ -9,5 +10,6 @@ System.out.println("Estoy trabajando");
 
 public void metodoNuevo(){
 System.out.println("Soy un metodo nuevo")
+
 }
 }
