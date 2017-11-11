47c04181106ffb1aead14c5035db45098249b2c0
Jonathan Chavez <jonechav@espol.edu.ec>
2017-11-11 09:28:31 -0500

    Se creo la nueva clase

--- Dentista.java
+++ Dentista.java
@@ -11,5 +11,10 @@ System.out.println("Estoy trabajando");
 public void metodoNuevo(){
 System.out.println("Soy un metodo nuevo")
 
+public void newMethodDeveloper1(){
+System.out.println("Creado el 11/11/2017");
+System.out.println("Este metodo hace cosas nuevas");
+}
+
 }
 }
